30a6e39afe88bcffd424730db307f1458de0eba0
zarkus <lucasferlima8@gmail.com>
2021-12-01 13:38:55 -0300

    Added Error 404

--- src/server.ts
+++ src/server.ts
@@ -7,6 +7,10 @@ const server = express();
 server.use(express.json());
 server.use(mainRoutes);
 
+server.use((req: Request, res: Response) => {
+    res.status(404).send("Página não encontrada");
+});
+
 // Turn on the server
 server.listen(8080, () => {
     return console.log("Server is running...");
